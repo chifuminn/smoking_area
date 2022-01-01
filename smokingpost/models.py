69c6df7f24fbca3b8cd8a4b369484f4dd152084a
chifuminn <chkr0503mama@gmail.com>
2022-01-01 17:50:01 +0900

    Modelsの追加

--- smokingpost/models.py
+++ smokingpost/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SmokingAreaModel(models.Model):
+    name=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    attention=models.TextField()
+    start_usage_time=models.TimeField()
+    end_usage_time=models.TimeField()
+    postdata=models.DateField(auto_now_add=True)
