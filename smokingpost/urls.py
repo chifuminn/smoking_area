7673677c8f4084f58cdc152e0eb51b96904a444d
chifuminn <chkr0503mama@gmail.com>
2022-01-08 09:53:34 +0900

    年齢確認ページの追加

--- smokingpost/urls.py
+++ smokingpost/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, age_confirmation_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('age_confirmation/', age_confirmation_view, name='age_confirmation'),
 ]
--- smokingpost/views.py
+++ smokingpost/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index_view(request):
     return render(request, 'index.html')
 
+def age_confirmation_view(request):
+    return render(request, 'age_confirmation.html')
+
