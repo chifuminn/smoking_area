fbc1aadff311a892297620b37447beb1c9d23d46
chifuminn <chkr0503mama@gmail.com>
2022-01-12 21:31:43 +0900

    リストを返した

--- smokingpost/views.py
+++ smokingpost/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import SmokingAreaModel
 
 def index_view(request):
-    return render(request, 'index.html')
+    smoking_area_list = SmokingAreaModel.objects.all()
+    return render(request, 'index.html',{'smoking_area_list':smoking_area_list})
 
 def age_confirmation_view(request):
     return render(request, 'age_confirmation.html')
