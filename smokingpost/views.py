35ffed9f453a91f7024fd1bd781a9c370900f4d4
chifuminn <chkr0503mama@gmail.com>
2022-02-17 19:51:06 +0900

    検索機能の追加

--- smokingpost/views.py
+++ smokingpost/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import SmokingAreaModel
+from django.contrib import messages
+from django.db.models import Q
 
 def age_confirmation_view(request):
     return render(request, 'age_confirmation.html')
@@ -7,6 +9,14 @@ def age_confirmation_view(request):
 def top_view(request):
     # TODO: 最新6件を表示したい
     smoking_area_list = SmokingAreaModel.objects.order_by("-postdate")
+    keyword = request.GET.get('keyword')
+
+    if keyword:
+        smoking_area_list = smoking_area_list.filter(
+            Q(name__icontains=keyword)
+        )
+        messages.success(request, '「{}」の検索結果'.format(keyword))
+    
     return render(request, 'top.html',{'smoking_area_list':smoking_area_list})
 
 def detail_view(request, pk):
